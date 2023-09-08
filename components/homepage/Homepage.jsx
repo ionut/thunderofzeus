f66e211ad94d9682e64f35165a0c7eb4384fb000
ionut <ionuthusoschi@gmail.com>
2023-09-08 18:37:45 +0300

    new updated lp

---
+++ components/homepage/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FirstSection from '../sections/FirstSection'
+
+const Homepage = () => {
+  return (
+   <>
+   <FirstSection/>
+   </>
+  )
+}
+
+export default Homepage
